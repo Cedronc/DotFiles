233e3d673140aa7f94a39e9bd3368dab7bee254a
Cedric <cedricmrtl@gmail.com>
2025-04-28 15:08:17 +0000

    added install script + updated gitignore

---
+++ install-base.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+sudo pacman -Syu git github-cli neovim neofetch lazygit npm firefox copyq
+
+git config --global user.email "cedricmrtl@gmail.com"
+git config --global user.name "Cedric"
+
